fba2d21e6377ee9e60ce62315a9b0a455742939f
Jiří H <jiri@bacehub.de>
2024-03-23 23:14:29 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,156 @@
-use ansi_term::{Style, Colour::Fixed};
+use ansi_term::{Colour::Fixed, Style};
+use itertools::Itertools;
+use std::collections::{BTreeMap, HashMap};
 use zellij_tile::prelude::*;
 
-use std::collections::{HashMap, BTreeMap};
-
 #[derive(Default)]
-struct State {
-    mode_log: HashMap<String, usize>,
-    tabs: Vec<String>,
-    test_runs: usize,
-    userspace_configuration: BTreeMap<String, String>,
+struct JumpPane {
+    title: String,
+    id: u32,
 }
 
-register_plugin!(State);
+#[derive(Default)]
+struct PluginState {
+    panes: HashMap<String, JumpPane>,
+    label_input: String,
+    label_alphabet: Vec<char>,
+}
 
-impl ZellijPlugin for State {
+register_plugin!(PluginState);
+
+impl PluginState {
+    fn clear(&mut self) {
+        self.label_input.clear();
+        self.panes.clear();
+    }
+}
+
+impl ZellijPlugin for PluginState {
     fn load(&mut self, configuration: BTreeMap<String, String>) {
-        self.userspace_configuration = configuration;
-        // we need the ReadApplicationState permission to receive the ModeUpdate and TabUpdate
-        // events
-        // we need the RunCommands permission to run "cargo test" in a floating window
-        request_permission(&[PermissionType::ReadApplicationState, PermissionType::RunCommands]);
-        subscribe(&[EventType::ModeUpdate, EventType::TabUpdate, EventType::Key]);
+        self.label_alphabet = configuration
+            .get("label_alphabet")
+            .map(|alphabet| alphabet.trim().to_lowercase())
+            // todo: probly qwerty default
+            .unwrap_or("tnseriaoplfuwyzkbvm".to_string())
+            .chars()
+            .collect();
+
+        // todo: tidy-up permissions
+        request_permission(&[
+            PermissionType::ReadApplicationState,
+            PermissionType::RunCommands,
+            PermissionType::ChangeApplicationState,
+        ]);
+        subscribe(&[EventType::Key, EventType::PaneUpdate]);
     }
 
     fn update(&mut self, event: Event) -> bool {
-        let mut should_render = false;
         match event {
-            Event::ModeUpdate(mode_info) => {
-                let mode = format!("{:?}", mode_info.mode);
-                let count = self.mode_log.entry(mode).or_insert(0);
-                *count += 1;
-                should_render = true;
+            Event::Key(Key::Esc) => {
+                hide_self();
+                self.clear();
+                return true;
             }
-            Event::TabUpdate(tab_info) => {
-                self.tabs = tab_info.iter().map(|t| t.name.clone()).collect();
-                should_render = true;
+            Event::Key(Key::Backspace) | Event::Key(Key::Delete) => {
+                self.label_input.pop();
+                return true;
             }
-            Event::Key(key) => {
-                if let Key::Char('n') = key {
-                    self.test_runs += 1;
-                    open_command_pane_floating(CommandToRun {
-                        path: "cargo".into(),
-                        args: vec!["test".to_owned()],
-                        cwd: None,
-                    });
+            Event::Key(Key::Char(c)) => {
+                self.label_input.push(c);
+
+                if let Some(pane) = self.panes.get(&self.label_input) {
+                    // pane selected
+                    focus_terminal_pane(pane.id, false);
+                    hide_self();
+                    self.clear();
                 }
+
+                return true;
             }
-            _ => (),
+            Event::Key(_) => {}
+            Event::PaneUpdate(PaneManifest { panes }) => {
+                self.panes.clear();
+                let visible_panes: Vec<_> = panes
+                    .values()
+                    .flatten()
+                    .filter(|p| p.is_selectable)
+                    .collect();
+                let label_len = if visible_panes.len() <= self.label_alphabet.len() {
+                    1
+                } else {
+                    2
+                };
+
+                // todo: exclude self && skip when the plugin pane is hidden
+                let mut unprocessed_panes = Vec::new();
+                for pane in visible_panes {
+                    let preffered_label = pane
+                        .title
+                        .chars()
+                        .take(label_len)
+                        .collect::<String>()
+                        .to_lowercase();
+                    if !self.panes.contains_key(&preffered_label)
+                        && preffered_label
+                            .chars()
+                            .all(|c| self.label_alphabet.contains(&c))
+                    {
+                        self.panes.insert(
+                            preffered_label,
+                            JumpPane {
+                                id: pane.id,
+                                title: pane.title.clone(),
+                            },
+                        );
+                    } else {
+                        unprocessed_panes.push(pane);
+                    }
+                }
+
+                if !unprocessed_panes.is_empty() {
+                    let mut alpha = self.label_alphabet.iter().permutations(label_len);
+                    for pane in unprocessed_panes {
+                        while let Some(label) = alpha.next() {
+                            let label = label.iter().cloned().collect();
+                            if !self.panes.contains_key(&label) {
+                                self.panes.insert(
+                                    label,
+                                    JumpPane {
+                                        id: pane.id,
+                                        title: pane.title.clone(),
+                                    },
+                                );
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+            _ => unimplemented!("{event:?}"),
         };
-        should_render
+
+        false
     }
 
-    fn render(&mut self, rows: usize, cols: usize) {
-        let colored_rows = color_bold(CYAN, &rows.to_string());
-        let colored_cols = color_bold(CYAN, &cols.to_string());
-        println!("");
-        println!("I have {} rows and {} columns", colored_rows, colored_cols);
-        println!("");
-        println!("{} {:#?}", color_bold(GREEN, "I was started with the following user configuration:"), self.userspace_configuration);
-        println!("");
-        println!("{}", color_bold(GREEN, "Modes:"));
-        for (mode, count) in &self.mode_log {
-            let count = color_bold(ORANGE, &count.to_string());
-            println!("{} -> Changed {} times", mode, count);
-        }
-        println!("");
-        let current_tabs = color_bold(GREEN, "Current Tabs:");
-        let comma = color_bold(ORANGE, ", ");
-        println!("{} {}", current_tabs, self.tabs.join(&comma));
-        println!("");
-        if self.test_runs > 0 {
-            let test_run_count = color_bold(CYAN, &self.test_runs.to_string());
-            println!("Ran tests {} times!", test_run_count);
+    // todo: use theme colours
+    fn render(&mut self, _rows: usize, _cols: usize) {
+        // title
+        println!(
+            "{}",
+            Style::new().fg(Fixed(GREEN)).bold().paint("Jump List\n")
+        );
+
+        // list
+        for (label, pane) in self.panes.iter() {
+            let label =
+                if !self.label_input.trim().is_empty() && label.starts_with(&self.label_input) {
+                    format!("{}", color_bold(GREEN, &self.label_input))
+                } else {
+                    color_bold(RED, label)
+                };
+            println!("[{label}] {}", pane.title);
         }
     }
 }
